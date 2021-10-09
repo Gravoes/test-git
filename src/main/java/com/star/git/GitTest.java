455e783fe69c7016c90afe2acb96b0e8b0bc3b13
jaqen <zjt7023521@163.com>
2021-10-09 20:59:23 +0800

    first commit

---
+++ src/main/java/com/star/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.star.git;
+
+/**
+ * @author Jaqen
+ * @create 2021-10-09-20:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
